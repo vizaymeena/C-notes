3a161d1d5798b1e822df86f1177d9b6a33cbab60
vizaymeena <vizaymeena@gmail.com>
2025-01-08 11:14:50 +0530

    Date: 08/jan/2025

--- Functions in c++/structor/structor.cpp
+++ Functions in c++/structor/structor.cpp
@@ -31,3 +31,34 @@ int main ()
     
     a.show();
 }
+
+
+// refrencing and derefrencing
+
+//Date: 08/JAN/2025
+#include <iostream>
+using namespace std;
+
+struct  employee{
+    int salary;
+    string name;
+
+}; // terminator required
+int main()
+{
+    employee p,c,b;
+    p.name="vijay";
+    p.salary=50000;
+
+    employee *a;  // *a holding address of another variable (pointer)
+    a=&p;         // a is holding address of p
+
+    cin>>a->name;  
+    cin>>a->salary;  
+    
+
+    cout<<a->name;   // calling a gives the value/address of p
+    cout<<endl;
+    cout<<a->salary;  // "->" is called "this" operator
+}
+
